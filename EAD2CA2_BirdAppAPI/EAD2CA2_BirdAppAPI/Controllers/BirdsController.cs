c38aaa48e40c81cc91d1535a9f86903e5f0c8500
Mateus Cardoso <X00149064@mytudublin.ie>
2021-03-24 14:14:30 +0000

    Get birds by family name now on BirdsController

--- EAD2CA2_BirdAppAPI/EAD2CA2_BirdAppAPI/Controllers/BirdsController.cs
+++ EAD2CA2_BirdAppAPI/EAD2CA2_BirdAppAPI/Controllers/BirdsController.cs
@@ -93,5 +93,21 @@ namespace EAD2CA2_BirdAppAPI.Controllers
             }
         }
 
+
+        [Route("familyName")]
+        [HttpGet]
+        public ActionResult<string> Get(string familyName)
+        {
+            try
+            {
+                var familyToGet = _dbContext.Birds.Where(b => b.familyName.Equals(familyName)).AsEnumerable();
+                return Ok(familyToGet);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Failed to Delete.");
+            }
+        }
+
     }
 }
