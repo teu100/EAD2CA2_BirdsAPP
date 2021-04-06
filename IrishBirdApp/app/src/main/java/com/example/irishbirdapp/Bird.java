ab1a0945844b750d28da9033831f60cfd967514a
James Fleming <X00149830@mytudublin.ie>
2021-04-06 20:33:18 +0100

    Added the Birds Class to the Android App

---
+++ IrishBirdApp/app/src/main/java/com/example/irishbirdapp/Bird.java
@@ -0,0 +1,20 @@
+package com.example.irishbirdapp;
+
+public class Bird {
+
+    private String commonName;
+    private String binomial;
+    private String irishName;
+    private String orderName;
+    private String familyName;
+    private String infoLink;
+    private byte liked;
+    private String imageLink;
+
+    public String toString()
+    {
+        return "To String to be finished";
+    }
+
+
+}
